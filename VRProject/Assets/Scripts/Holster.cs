0cb177a63e07e46dd2ce8f4cd5cdbe3b1a779e34
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-11-09 13:40:37 +0100

    update to holster

---
+++ VRProject/Assets/Scripts/Holster.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holster : MonoBehaviour
+{
+    Color ogColor;
+    [SerializeField] Material collisiionColor;
+    void Start()
+    {
+        ogColor = gameObject.GetComponent<Renderer>().material.color;
+        Debug.Log(ogColor.ToString());
+    }
+
+    void OnTriggerEnter(Collider objectName)
+    {
+        Debug.Log("Entered collision with " + objectName.gameObject.name);
+    }
+
+    void OnTriggerStay(Collider objectName)
+    {
+        Debug.Log("Colliding with " + objectName.gameObject.name);
+        gameObject.GetComponent<Renderer>().material.color = collisiionColor.color;
+    }
+
+    void OnTriggerExit(Collider objectName)
+    {
+        Debug.Log("Exited collision with " + objectName.gameObject.name);
+        gameObject.GetComponent<Renderer>().material.color = ogColor;
+    }
+}
