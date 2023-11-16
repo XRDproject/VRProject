6f2d91c78b651b87d264de3243e5848e8c9c55ee
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-11-16 14:50:52 +0100

    Pause menu

---
+++ VRProject/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenu;
+    [Tooltip("Specify pause button")][SerializeField] InputActionReference pauseButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pauseButton.action.started += ctx =>
+        {
+              if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        };
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
